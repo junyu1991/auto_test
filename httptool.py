4edec8f3a3011fb1ec00cf8d22fd8c4c97c57853
yujun <juny1991@163.com>
2018-01-29 18:01:57 +0800

    Change the header process method

--- httptool.py
+++ httptool.py
@@ -9,6 +9,7 @@ import json
 import sys
 import urlparse
 import datetime
+import re
 
 import requests
 
@@ -20,7 +21,25 @@ class URL:
     def __init__(self,url,header={},data={}):
         self._url=url
         self._header=header
-        self._data=data
+        self._data=self._handle_data(data)
+
+    def _handle_data(self,data):
+        '''
+        Process the data from json file,turn the dict data to tuple data.
+        return tuple
+        '''
+        result=[]
+        for k in data.iterkeys():
+            value=data.get(k)
+            if isinstance(value,list):
+                for v in value:
+                    result.append((k,v))
+            else:
+                result.append((k,value))
+        return tuple(result)
+                
+            
+    
     def get_url(self):
         return self._url
     def get_header(self):
@@ -31,16 +50,26 @@ class URL:
     def __str__(self):
         return "%s\t%s\t%s" % (str(self._url),str(self._header),str(self._data))
 
-def get_post_urls(json_data):
-    post_dict=json_data.get('post')
+def get_post_urls(json_data,method_name='post'):
+    '''
+    Get url configed in json file
+    method_name:get or post
+    '''
+    post_dict=json_data.get(method_name)
     result=[]
     for key in post_dict.iterkeys():
         url=key
         header=post_dict.get(key).get('header')
         data=post_dict.get(key).get('data')
+        print data
         result.append(URL(url,header,data))
 
     return result
+
+
+def get_regx():
+    return re.compile('(?<=parent\.submitCallBack).*(?=;)')
+
 
 def get_json_data(json_file):
     if os.path.exists(json_file):
@@ -68,6 +97,7 @@ def save_result(save_data):
         print save_path
         os.makedirs(save_path)
     file_name=os.path.join(save_path,datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S")+'.txt')
+	
     print("Saving data to file :%s" % file_name)
     
     with open(file_name,'wb') as f:
@@ -76,6 +106,10 @@ def save_result(save_data):
         f.write(str(save_data.headers))
         f.write("\n")
         f.write(save_data.text.encode('UTF-8'))
+    print("\n"*3)
+    re_result=get_regx().search(save_data.text)
+    if re_result:
+        print re_result.group()
 
 def post_url(url_list):
     '''
@@ -83,19 +117,44 @@ def post_url(url_list):
     url_list:The list of URL class
     '''
     for U in url_list:
-        print("Post data to %s" % U.get_url())
+        print("Send post requests %s" % U.get_url())
         print U
+        #print U.get_data()
         r=requests.post(url=U.get_url(),data=U.get_data(),headers=U.get_header())
         save_result(r)
 
+def get_url_data(url_list):
+    '''
+    Send GET request
+    '''
+    for U in url_list:
+        print("Send get requests %s" % U.get_url())
+        r=requests.get(url=U.get_url(),params=U.get_data(),headers=U.get_header())
+        save_result(r)
+
+def usage():
+    usage='''
+        python httptool.py -c [json config file,default ./data/url.json]
+                           -m [choose the method to send data,which configed in json file,post:the post url data;get: the get url data;all: send all requests configed in json file]
+                           -h/--help usage()
+        '''
+    print(usage)
+
 def main():
-    opts,args=getopt.getopt(sys.argv[1:],'hc:d:',['help'])
+    opts,args=getopt.getopt(sys.argv[1:],'hc:m:',['help'])
     json_file="./data/url.json"
+    method='post'
     for opt,value in opts:
         if opt=='-c':
             json_file=value
+        if opt=='-m':
+            method=str(value).lower()
+            
     print json_file
-    post_url(get_post_urls(get_json_data(json_file)))
+    if(method=='post'):
+        post_url(get_post_urls(get_json_data(json_file)))
+    elif(method=='get'):
+        get_url_data(get_post_urls(get_json_data(json_file),'get'))
 
 if __name__=='__main__':
     main()
--- utils.py
+++ utils.py
@@ -24,6 +24,26 @@ def save_img(url):
 
     return file_name
 
+def save_img(url,connection):
+    '''
+    Save img to the disk,use requests.session() to connect url to get data.
+    '''
+    r=connection.get(url,stream=True)
+    file_name=datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
+    dirs=os.path.join(os.curdir,"temp")
+    if not os.path.exists(dirs):
+        print("Make dirs:%s" % dirs)
+        os.makedirs(dirs)
+
+    file_name=os.path.join(dirs,file_name)
+    print("Saving img to:%s" % file_name)
+
+    with open(file_name,'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024*1024):
+            f.write(chunk)
+
+    return file_name
+
 def show_img(img_path):
     if os.path.exists(img_path):
         lena=mpimg.imread(img_path)
